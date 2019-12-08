3fbd5ea1cca4dd95bea6defeb81b46f620c9cd1b
pBinarySoul <pBinarySoul@gmail.com>
2019-12-08 18:45:00 +0300

    Now tab prints a nice message if something went wrongd

--- lua/quest/vgui/DQuestsPanel.lua
+++ lua/quest/vgui/DQuestsPanel.lua
@@ -21,43 +21,53 @@ function PANEL:Init( )
 		if ( table.Count(TTTQuests.MyQuests) > 0 && table.Count(TTTQuests.MyQuestsData) > 0 ) then
 			for _, class in pairs(TTTQuests.MyQuests) do
 				local Quest = TTTQuests.Quests[class]
-				local item = vgui.Create("DQuestItem", self.Scroll, class)
-				local typesIcons = {
-					[TTTQuests.RewardType.StandardPoints] = "pointshop2/dollar103.png",
-					[TTTQuests.RewardType.PremiumPoints] = "materials/pointshop2/donation.png",
-					[TTTQuests.RewardType.Item] = "ps-quests/icon-item.png",
-					[TTTQuests.RewardType.Experience] = "materials/pointshop2/settings12.png",
-					[TTTQuests.RewardType.Random] = "ps-quests/icon-rand.png",
-				}
-				
-				item:SetQuestClass(class)
-				item:SetSize(260, 190)
-				item:SetHeaderText(Quest.Name)
-				item:SetDescText(string.format(Quest.Description, Quest.TargetValue))
 
-				item.RewardType:SetImage(typesIcons[Quest.RewardType] || "ps-quests/icon-rand.png")
-				item.RewardType:SetPos(item:GetWide() / 2 - 16, 0)
+				if Quest then
+					local item = vgui.Create("DQuestItem", self.Scroll, class)
+					local typesIcons = {
+						[TTTQuests.RewardType.StandardPoints] = "pointshop2/dollar103.png",
+						[TTTQuests.RewardType.PremiumPoints] = "materials/pointshop2/donation.png",
+						[TTTQuests.RewardType.Item] = "ps-quests/icon-item.png",
+						[TTTQuests.RewardType.Experience] = "materials/pointshop2/settings12.png",
+						[TTTQuests.RewardType.Random] = "ps-quests/icon-rand.png",
+					}
+					
+					item:SetQuestClass(class)
+					item:SetSize(260, 190)
+					item:SetHeaderText(Quest.Name)
+					item:SetDescText(string.format(Quest.Description, Quest.TargetValue))
 
-				if ( Quest.RewardType == TTTQuests.RewardType.Random ) then
-					item.RewardTypePanel:SetTooltip("You'll receive some random stuff")
-				elseif ( Quest.RewardType == TTTQuests.RewardType.Item ) then
-					item.RewardTypePanel:SetTooltip(string.format("You'll receive %s", TTTQuests.GetItemNameByClass(Quest.Reward)))
-				elseif ( Quest.RewardType == TTTQuests.RewardType.StandardPoints ) then
-					item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Standard Points", Quest.Reward))
-				elseif ( Quest.RewardType == TTTQuests.RewardType.PremiumPoints ) then
-					item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Donator Points", Quest.Reward))
-				elseif ( Quest.RewardType == TTTQuests.RewardType.Experience ) then
-					item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Experience", Quest.Reward))
+					item.RewardType:SetImage(typesIcons[Quest.RewardType] || "ps-quests/icon-rand.png")
+					item.RewardType:SetPos(item:GetWide() / 2 - 16, 0)
+
+					if ( Quest.RewardType == TTTQuests.RewardType.Random ) then
+						item.RewardTypePanel:SetTooltip("You'll receive some random stuff")
+					elseif ( Quest.RewardType == TTTQuests.RewardType.Item ) then
+						item.RewardTypePanel:SetTooltip(string.format("You'll receive %s", TTTQuests.GetItemNameByClass(Quest.Reward)))
+					elseif ( Quest.RewardType == TTTQuests.RewardType.StandardPoints ) then
+						item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Standard Points", Quest.Reward))
+					elseif ( Quest.RewardType == TTTQuests.RewardType.PremiumPoints ) then
+						item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Donator Points", Quest.Reward))
+					elseif ( Quest.RewardType == TTTQuests.RewardType.Experience ) then
+						item.RewardTypePanel:SetTooltip(string.format("You'll receive %s Experience", Quest.Reward))
+					end
+
+					item.ProgressBar:SetMax(Quest.TargetValue)
+					item.ProgressBar:SetCurrent(TTTQuests.MyQuestsData[class])
+					item.ProgressBar:Dock(BOTTOM)
+
+					item.ProgressBar.ProgressBar:SetSize(item:GetWide() - 24 * 2, 40)
+					item.ProgressBar.ProgressBar:SetPos(item:GetWide() / 2 - item.ProgressBar.ProgressBar:GetWide() / 2, 0 )
+
+					self.IconLayout:Add(item)
+				else
+					local errorText = vgui.Create("DLabel", self)
+					errorText:SetText("Something went wrong. Try to reconnect to the server!")
+					errorText:SetFont("PS2_LargeHeading")
+					errorText:SetTextColor(COLOR_WHITE)
+					errorText:Dock(FILL)
+					errorText:SetContentAlignment(8)
 				end
-
-				item.ProgressBar:SetMax(Quest.TargetValue)
-				item.ProgressBar:SetCurrent(TTTQuests.MyQuestsData[class])
-				item.ProgressBar:Dock(BOTTOM)
-
-				item.ProgressBar.ProgressBar:SetSize(item:GetWide() - 24 * 2, 40)
-				item.ProgressBar.ProgressBar:SetPos(item:GetWide() / 2 - item.ProgressBar.ProgressBar:GetWide() / 2, 0 )
-
-				self.IconLayout:Add(item)
 			end
 			timer.Destroy("DQuestsPanel_Timer")
 		end
